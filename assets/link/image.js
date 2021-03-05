82b50fc44e268ea1aa570f631642da7f06dc5c37
Dima Zaporozhets <dimkazaporozhets@gmail.com>
2021-03-05 09:53:11 +0200

    fonts

---
+++ assets/link/image.js
@@ -0,0 +1,9 @@
+export const tabScreen = {
+  uri:
+    'https://cdn.pixabay.com/photo/2020/06/12/08/33/mountain-5289671_960_720.jpg',
+};
+
+export const profileScreen = {
+  uri:
+    'https://cdn.pixabay.com/photo/2020/10/22/19/26/rocks-5676982_960_720.jpg',
+};
